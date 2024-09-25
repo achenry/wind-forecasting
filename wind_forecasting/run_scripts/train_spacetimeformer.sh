d20a3f8809599c83ac2a3cc3af6e7ea683e62507
Juan Boullosa <Jmb0507@gmail.com>
2024-09-24 19:25:32 -0600

    Retrack everything

---
+++ wind_forecasting/run_scripts/train_spacetimeformer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH --account=ssc
+#SBATCH --time=01:00:00
+#SBATCH --mem=80G
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --output=%j-%x.log
+
+module purge
+ml mamba
+mamba activate wind_forecasting_env
+
+python train_spacetimeformer.py spacetimeformer windfarm --debug --run_name spacetimeformer_windfarm_debug --context_points 600 --target_points 600
