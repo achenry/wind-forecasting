42dedf1fa91368ab6d1a5544ecbcc85fb4434498
Juan Boullosa <Jmb0507@gmail.com>
2024-09-30 16:02:26 -0600

    ...

--- install_rc/install.sh
+++ install_rc/install.sh
@@ -2,18 +2,12 @@
 
 # Instructions for installing environments on the CU Boulder Alpine cluster (for jubo7621)
 # Assumes user has already logged in via SSH and acompile as follows:
+#
 # ssh jubo7621@login.rc.colorado.edu
 # acompile
 
 module purge
 ml mambaforge
-
-# Check if mamba is available
-if ! command -v mamba &> /dev/null
-then
-    echo "mamba could not be found. Please check if the mambaforge module is loaded correctly."
-    exit 1
-fi
 
 # Create and activate environments
 for env_file in wind_forecasting_cuda_test.yml wind_forecasting_env_test.yml wind_forecasting_rocm_test.yml wind_preprocessing_test.yml
@@ -25,15 +19,7 @@ do
     mamba deactivate
 done
 
-# Clone the repository
-cd /projects/$USER
-if [ ! -d "/projects/$USER/wind-forecasting" ]; then
-    echo "Cloning wind-forecasting repository..."
-    git clone --recurse-submodules https://github.com/achenry/wind-forecasting.git
-else
-    echo "wind-forecasting repository already exists. Skipping clone."
-fi
-cd wind-forecasting/wind-forecasting/models
+cd /projects/$USER/wind-forecasting/wind-forecasting/models
 
 # ***Uncomment these lines if you need to install requirements for specific models***
 # python -m pip install -r ./spacetimeformer/requirements.txt
