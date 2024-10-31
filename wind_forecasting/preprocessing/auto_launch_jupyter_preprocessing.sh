0c3c72a87a50239dfc495492b1960b2f9ee22e60
Aoife Henry <ahenry@kl2.head.cm.kestrel.hpc.nrel.gov>
2024-10-31 11:28:30 -0600

    updates

--- wind_forecasting/preprocessing/auto_launch_jupyter_preprocessing.sh
+++ wind_forecasting/preprocessing/auto_launch_jupyter_preprocessing.sh
@@ -6,7 +6,7 @@ set -e
 unset XDF_RUNTIME_DIR
 
 # Use full CPU node script by default. For a shared partition or GPU node, comment out the next line and uncomment the line corresponding to the script you would like to run.
-RES=$(sbatch sbatch_jupyter.sh)
+RES=$(sbatch sbatch_jupyter_preprocessing.sh)
 # RES=$(sbatch shared_sbatch_jupyter.sh)
 # RES=$(sbatch gpu_sbatch_jupyter.sh) 
 
