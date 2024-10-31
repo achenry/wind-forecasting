40d82876cf77703df1e66942c94cd27619cb6463
Aoife Henry <aoife.henry@colorado.edu>
2024-10-31 13:12:53 -0600

    updates

--- wind_forecasting/preprocessing/load_data.sh
+++ wind_forecasting/preprocessing/load_data.sh
@@ -4,22 +4,18 @@
 #SBATCH --ntasks=104
 ##SBATCH --mem=0
 #SBATCH --account=ssc
-#SBATCH --time=01:00:00
-#SBATCH --partition=standard
+#SBATCH --time=02:00:00
+#SBATCH --partition=bigmem
 #SBATCH --output=data_loader.out
 
 module purge
 module load mamba
 mamba activate wind_forecasting_env
-#module load gcc
-#module load openmpi
-#module load intel
-#module load impi
 
-#export LD_LIBRARy_PATH=$CONDA_PREFIX/lib
+export LD_LIBRARy_PATH=$CONDA_PREFIX/lib
 
-#mpirun -np $SLURM_NTASKS python data_loader.py
-python data_loader.py
+mpirun -np $SLURM_NTASKS python data_loader.py
+#python data_loader.py
 
 #rm /pl/active/paolab/awaken_data/kp.turbine.z02.b0/*.nc
 #mv /scratch/alpine/aohe7145/awaken_data/kp.turbine.zo2.b0.raw.parquet /pl/active/paolab/awaken_data/kp.turbine.zo2.b0.raw.parquet
