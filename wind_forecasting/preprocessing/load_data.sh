f3a67f8e8573a760b8a1a8b317ccdb84513df7ba
Aoife Henry <ahenry@kl2.head.cm.kestrel.hpc.nrel.gov>
2024-11-05 09:41:25 -0700

    kestel updates

--- wind_forecasting/preprocessing/load_data.sh
+++ wind_forecasting/preprocessing/load_data.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=104
+#SBATCH --ntasks=52
 ##SBATCH --mem=0
 #SBATCH --account=ssc
-#SBATCH --time=02:00:00
-#SBATCH --partition=bigmem
+#SBATCH --time=01:00:00
+##SBATCH --partition=bigmem
+#SBATCH --partition=standard
 #SBATCH --output=data_loader.out
 
 module purge
 module load mamba
 mamba activate wind_forecasting_env
+echo $SLURM_NTASKS
 
+export MPICH_SHARED_MEM_COLL_OPT=mpi_bcast,mpi_barrier 
+export MPICH_COLL_OPT_OFF=mpi_allreduce 
 export LD_LIBRARy_PATH=$CONDA_PREFIX/lib
 
 mpirun -np $SLURM_NTASKS python data_loader.py
