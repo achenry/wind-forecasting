3842301aa17d59194cb4702cbeadd0d76b285c9f
Aoife Henry <aoife.henry@colorado.edu>
2025-01-13 11:52:37 -0700

    updated install scripts

--- install_scripts/install_kestrel.sh
+++ install_scripts/install_kestrel.sh
@@ -5,7 +5,7 @@ ssh ahenry@kestrel-gpu.hpc.nrel.gov
 # rm -rf /home/ahenry/.conda-pkgs/cache
 ml PrgEnv-intel
 ml mamba
-mamba create --prefix=/projects/ssc/ahenry/conda/envs/wind_forecasting --y
+mamba create --prefix=/projects/aohe7145/software/anaconda/envs/wind_forecasting --y
 mamba activate wind_forecasting
 mamba install conda-forge::cuda-version=12.4 nvidia/label/cuda-12.4.0::cuda-toolkit performer-pytorch pytorch torchvision torchaudio torchmetrics pytorch-cuda=12.4 lightning -c pytorch -c nvidia --y
 mamba install polars windrose statsmodels scikit-learn jupyterlab nb_conda_kernels pyyaml matplotlib numpy seaborn opt_einsum netcdf4 scipy h5pyd pyarrow wandb einops --y 
@@ -13,13 +13,10 @@ pip install mpi4py impi_rt opencv-python floris
 
 git clone https://github.com/achenry/wind-forecasting.git
 cd wind_forecasting
+git checkout feature/spacetimeformer
 python setup.py develop
 cd ..
 
-git clone https://github.com/achenry/OpenOA
-cd OpenOA
-pip install -e .
-cd ..
 
 git clone https://github.com/boujuan/pytorch-transformer-ts
 cd pytorch-transformer-ts
@@ -31,6 +28,11 @@ cd ..
 git clone https://github.com/achenry/gluonts
 cd gluonts
 git checkout mv_prob
+pip install -e .
+cd ..
+
+git clone https://github.com/achenry/OpenOA
+cd OpenOA
 pip install -e .
 cd ..
 
