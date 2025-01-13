94d5bdc5035642d9c21cdd331199397fffedbeb9
Aoife Henry <aoife.henry@colorado.edu>
2025-01-13 15:58:14 -0700

    fixed bug

--- install_scripts/transfer_to_kestrel.sh
+++ install_scripts/transfer_to_kestrel.sh
@@ -2,6 +2,7 @@ scp -r /Users/ahenry/Documents/toolboxes/wind-hybrid-open-controller/examples/fl
 
 scp -r /Users/ahenry/Downloads/downloader/downloader-7edb6ef4c06843d8928a1d74e ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/kp.turbine.z02.b0/
 
-scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/normalized_data.parquet ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
+scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/preprocessed_awaken_data/filled_data_normalized.parquet ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
+scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/preprocessed_awaken_data/filled_data_normalization_consts.csv ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
 
 scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/normalization_consts.csv ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
