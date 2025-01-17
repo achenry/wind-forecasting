cb10af1f889983ea94aa25ceacb350dc6ae4f452
Aoife Henry <aoife.henry@colorado.edu>
2025-01-17 15:44:22 -0700

    updates

--- wind_forecasting/preprocessing/load_data.sh
+++ wind_forecasting/preprocessing/load_data.sh
@@ -20,7 +20,9 @@ export RUST_BACKTRACE=full
 #export MPICH_COLL_OPT_OFF=mpi_allreduce 
 #export LD_LIBRARy_PATH=$CONDA_PREFIX/lib
 
-mpirun -np $SLURM_NTASKS python preprocessing_main.py --load_data
+mpirun -np $SLURM_NTASKS python preprocessing_main.py --config /$HOME/toolboxes/wind_forecasting_env/wind-forecasting/examples/inputs/preprocessing_inputs_kestrel_awaken_new.yaml --load_data
+
+# mpirun -np $SLURM_NTASKS python preprocessing_main.py --config /$HOME/toolboxes/wind_forecasting_env/wind-forecasting/examples/inputs/preprocessing_inputs_kestrel_awaken_new.yaml --load_data
 #python data_loader.py
 
 #rm /pl/active/paolab/awaken_data/kp.turbine.z02.b0/*.nc
