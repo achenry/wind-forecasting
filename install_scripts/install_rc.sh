0aa333b7fe79cd6b74635e35bf3ab8b603c79f7c
Aoife Henry <aoife.henry@colorado.edu>
2025-01-16 17:22:02 -0700

    upd

--- install_scripts/install_rc.sh
+++ install_scripts/install_rc.sh
@@ -1,10 +1,11 @@
 acompile
 ml mambaforge
-mamba create --prefix=/projects/aohe7145/software/anaconda/envs/wind_forecasting python=3.12.4 --y
+mamba create --prefix=/projects/aohe7145/software/anaconda/envs/wind_forecasting python=3.12 --y
 mamba activate wind_forecasting
+
 mamba install conda-forge::cuda-version=12.4 nvidia/label/cuda-12.4.0::cuda-toolkit performer-pytorch pytorch torchvision torchaudio torchmetrics pytorch-cuda=12.4 lightning -c pytorch -c nvidia --y
-mamba install polars windrose statsmodels scikit-learn jupyterlab nb_conda_kernels pyyaml matplotlib numpy seaborn opt_einsum netcdf4 scipy h5pyd pyarrow wandb einops cython --y 
-pip3 install mpi4py impi_rt opencv-python floris
+mamba install polars windrose statsmodels scikit-learn jupyterlab nb_conda_kernels pyyaml matplotlib numpy seaborn opt_einsum netcdf4 scipy h5pyd wandb einops --y 
+python -m pip install mpi4py impi_rt opencv-python floris 
 
 git clone https://github.com/achenry/wind-forecasting.git
 cd wind-forecasting
@@ -15,14 +16,15 @@ cd ..
 git clone https://github.com/achenry/gluonts
 cd gluonts
 git checkout mv_prob
-pip3 install -e .[dev]
+./dev_setup.sh
+python -m pip install -e .
 cd ..
 
 git clone https://github.com/boujuan/pytorch-transformer-ts
 cd pytorch-transformer-ts
 git checkout feature/spacetimeformer
-pip3 install ujson datasets xformers etsformer-pytorch reformer_pytorch pykeops apex # gluonts[torch]
-pip3 install git+https://github.com/kashif/hopfield-layers@pytorch-2 git+https://github.com/microsoft/torchscale
+python -m pip install ujson datasets xformers etsformer-pytorch reformer_pytorch pykeops apex # gluonts[torch]
+python -m pip install git+https://github.com/kashif/hopfield-layers@pytorch-2 git+https://github.com/microsoft/torchscale
 cd ..
 
 # git clone https://github.com/achenry/OpenOA
