d5ef7584913b566697e309ebda33b470b078398e
Aoife Henry <aoife.henry@colorado.edu>
2025-01-17 07:40:12 -0700

    input fix

--- install_scripts/install_rc.sh
+++ install_scripts/install_rc.sh
@@ -3,15 +3,33 @@ ml mambaforge
 mamba create --prefix=/projects/aohe7145/software/anaconda/envs/wind_forecasting python=3.12 --y
 mamba activate wind_forecasting
 
-mamba install conda-forge::cuda-version=12.4 nvidia/label/cuda-12.4.0::cuda-toolkit performer-pytorch pytorch torchvision torchaudio torchmetrics pytorch-cuda=12.4 lightning -c pytorch -c nvidia --y
+# mamba install conda-forge::cuda-version=12.4 nvidia/label/cuda-12.4.0::cuda-toolkit performer-pytorch pytorch torchvision torchaudio torchmetrics pytorch-cuda=12.4 lightning -c pytorch -c nvidia --y
 mamba install polars windrose statsmodels scikit-learn jupyterlab nb_conda_kernels pyyaml matplotlib numpy seaborn opt_einsum netcdf4 scipy h5pyd wandb einops --y 
-python -m pip install mpi4py impi_rt opencv-python floris 
+python -m pip install opencv-python floris performer-pytorch 
 
 git clone https://github.com/achenry/wind-forecasting.git
 cd wind-forecasting
 git checkout feature/spacetimeformer
 python setup.py develop
 cd ..
+
+# cd wind-forecasting
+# python setup.py develop
+# cd ../gluonts
+# ./dev_setup.sh
+# python -m pip install -e .
+# cd ../pytorch-transformer-ts
+# python -m pip install ujson datasets xformers etsformer-pytorch reformer_pytorch pykeops apex # gluonts[torch]
+# python -m pip install git+https://github.com/kashif/hopfield-layers@pytorch-2 git+https://github.com/microsoft/torchscale
+# cd ..
+
+git clone https://github.com/boujuan/pytorch-transformer-ts
+cd pytorch-transformer-ts
+git checkout feature/spacetimeformer
+python -m pip install ujson datasets xformers etsformer-pytorch reformer_pytorch pykeops apex gluonts[torch]
+python -m pip install git+https://github.com/kashif/hopfield-layers@pytorch-2 git+https://github.com/microsoft/torchscale
+cd ..
+
 
 git clone https://github.com/achenry/gluonts
 cd gluonts
@@ -20,12 +38,6 @@ git checkout mv_prob
 python -m pip install -e .
 cd ..
 
-git clone https://github.com/boujuan/pytorch-transformer-ts
-cd pytorch-transformer-ts
-git checkout feature/spacetimeformer
-python -m pip install ujson datasets xformers etsformer-pytorch reformer_pytorch pykeops apex # gluonts[torch]
-python -m pip install git+https://github.com/kashif/hopfield-layers@pytorch-2 git+https://github.com/microsoft/torchscale
-cd ..
 
 # git clone https://github.com/achenry/OpenOA
 # cd OpenOA
