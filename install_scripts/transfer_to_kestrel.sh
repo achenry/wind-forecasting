bf35aea4dbe546c352abded8cb0a6f698660f50d
Aoife Henry <aoife.henry@colorado.edu>
2025-01-21 07:47:47 -0700

    input pupdates'

--- install_scripts/transfer_to_kestrel.sh
+++ install_scripts/transfer_to_kestrel.sh
@@ -11,3 +11,6 @@ scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/preproce
 scp -r /srv/data/nfs/scada/00_engie_scada/ ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
 
 scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/normalization_consts.csv ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
+
+scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/preprocessed_flasc_data/SMARTEOLE_WakeSteering_SCADA_1minData_normalized.parquet ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
+scp -r /Users/ahenry/Documents/toolboxes/wind_forecasting/examples/data/preprocessed_flasc_data/SMARTEOLE_WakeSteering_SCADA_1minData_normalization_consts.csv ahenry@kestrel-gpu.hpc.nrel.gov:/projects/ssc/ahenry/wind_forecasting/awaken_data/
