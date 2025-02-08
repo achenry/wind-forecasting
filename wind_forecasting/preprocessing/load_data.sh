ca5660f830f504e6a9944c37381066732dc07081
Aoife Henry <ahenry@kl3.head.cm.kestrel.hpc.nrel.gov>
2025-02-08 04:45:19 -0700

    upd

--- wind_forecasting/preprocessing/load_data.sh
+++ wind_forecasting/preprocessing/load_data.sh
@@ -5,20 +5,20 @@
 #SBATCH --mem=0
 #SBATCH --account=ssc
 #SBATCH --time=06:00:00
-#SBATCH --partition=bigmem
+##SBATCH --partition=bigmem
 #SBATCH --partition=standard
 #SBATCH --output=load_data.out
-#SBATCH --tmp=1T
+##SBATCH --tmp=1T
 
 module purge
 module load mamba
 mamba activate wind_forecasting
 echo $SLURM_NTASKS
-export RUST_BACKTRACE=full
+# export RUST_BACKTRACE=full
 # salloc --partition=debug --mem=0 --time=00:30:00 --ntasks=104 --account=ssc
-#export MPICH_SHARED_MEM_COLL_OPT=mpi_bcast,mpi_barrier 
-#export MPICH_COLL_OPT_OFF=mpi_allreduce 
-#export LD_LIBRARy_PATH=$CONDA_PREFIX/lib
+export MPICH_SHARED_MEM_COLL_OPT=mpi_bcast,mpi_barrier 
+export MPICH_COLL_OPT_OFF=mpi_allreduce 
+export LD_LIBRARy_PATH=$CONDA_PREFIX/lib
 
 # cd $LARGE_STORAGE/ahenry/wind_forecasting_env/wind-forecasting/wind_forecasting/preprocessing
 # conda activate wind_forecasting_preprocessing
