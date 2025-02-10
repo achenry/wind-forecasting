1c329b62e12025fffdfd9da14ff20baf9eac6ad7
Aoife Henry <aoife.henry@colorado.edu>
2025-02-10 15:38:33 -0700

    upd

--- wind_forecasting/preprocessing/preprocessing_main.py
+++ wind_forecasting/preprocessing/preprocessing_main.py
@@ -185,10 +185,19 @@ def main():
             start_time = time.time()
     
     if args.reload_data or not os.path.exists(data_loader.save_path):
+        temp_save_dir = os.path.join(os.path.dirname(data_loader.save_path), os.path.basename(data_loader.save_path).replace(".parquet", "_temp"))
         if RUN_ONCE:
-            data_loader.make_paths()
+            logging.info(f"Making temporary directory {temp_save_dir}")
+            if os.path.exists(temp_save_dir):
+                rmtree(temp_save_dir)
+                # raise Exception(f"Temporary saving directory {temp_save_dir} already exists! Please remove or rename it.")
+            os.makedirs(temp_save_dir)
+        
+            if not os.path.exists(os.path.dirname(data_loader.save_path)):
+                logging.info(f"Making directory to save_path {os.path.dirname(data_loader.save_path)}")
+                os.makedirs(os.path.dirname(data_loader.save_path))
                         
-        df_query = data_loader.read_multi_files()
+        df_query = data_loader.read_multi_files(temp_save_dir)
         
         if RUN_ONCE:
             data_loader.remove_paths()
