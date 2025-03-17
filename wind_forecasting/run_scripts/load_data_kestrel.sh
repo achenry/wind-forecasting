1ace59cbc236655c9b28b14cf870be1e83254f2c
Aoife Henry <aoife.henry@colorado.edu>
2025-03-17 13:02:34 -0600

    upd

---
+++ wind_forecasting/run_scripts/load_data_kestrel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=104
+#SBATCH --mem=0
+#SBATCH --account=ssc
+#SBATCH --time=02:00:00
+#SBATCH --partition=bigmem
+#SBATCH --partition=standard
+#SBATCH --output=load_data.out
+#SBATCH --tmp=1T
+
+module purge
+module load mamba
+mamba activate wind_forecasting
+
+python load_data.py --config /$HOME/toolboxes/wind_forecasting_env/wind-forecasting/examples/inputs/training_inputs_kestrel_flasc.yaml --reload_data
