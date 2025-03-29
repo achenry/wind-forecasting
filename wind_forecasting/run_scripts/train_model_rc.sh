2ec72cc833384f434fbfb9c632bf58bacfc18158
Aoife Henry <aoife.henry@colorado.edu>
2025-03-29 15:18:13 -0600

    upd

--- wind_forecasting/run_scripts/train_model_rc.sh
+++ wind_forecasting/run_scripts/train_model_rc.sh
@@ -15,15 +15,19 @@ module load miniforge
 module load intel impi
 conda activate wind_forecasting
 
+export WANDB_API_KEY=a9aec8e98a88077de29031385225167c720030f7
+
 echo "SLURM_NTASKS=${SLURM_NTASKS}"
 echo "SLURM_JOB_NUM_NODES=${SLURM_JOB_NUM_NODES}"
 echo "SLURM_GPUS_ON_NODE=${SLURM_GPUS_ON_NODE}"
 export WANDB_API_KEY=a9aec8e98a88077de29031385225167c720030f7
 # echo "SLURM_JOB_GPUS=${SLURM_JOB_GPUS}"
 # echo "SLURM_JOB_GRES=${SLURM_JOB_GRES}"
+export PYTHON_EXECUTABLE=$(which python)
 
 # NOTE run the following first: python load_data.py --config ../../examples/inputs/training_inputs_rc_awaken.yaml --reload
-mpirun -np $SLURM_NTASKS python run_model.py --config ../../examples/inputs/training_inputs_rc_flasc.yaml --mode train --model $1 
+# mpirun -np $SLURM_NTASKS 
+srun $PYTHON_EXECUTABLE run_model.py --config ../../examples/inputs/training_inputs_rc_flasc.yaml --mode train --model $1 
 # srun python informer.py
 #python train_spacetimeformer.py spacetimeformer windfarm --debug --run_name spacetimeformer_windfarm_debug --context_points 600 --target_points 600
 
