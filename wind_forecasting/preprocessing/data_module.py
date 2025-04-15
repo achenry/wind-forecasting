7468c6031e07ec42879d3bf4d5fb4a4414d11bc5
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2025-04-15 13:24:59 +0200

    ~

--- wind_forecasting/preprocessing/data_module.py
+++ wind_forecasting/preprocessing/data_module.py
@@ -173,7 +173,7 @@ class DataModule():
         if splits is None:
             splits = ["train", "val", "test"]
         assert all(split in ["train", "val", "test"] for split in splits)
-        assert os.path.exists(self.train_ready_data_path), f"Must run generate_datasets before generate_splits to product {self.train_ready_data_path}."
+        assert os.path.exists(self.train_ready_data_path), f"Must run generate_datasets before generate_splits to produce {self.train_ready_data_path}."
         
         logging.info(f"Scanning dataset {self.train_ready_data_path}.") 
         dataset = IterableLazyFrame(data_path=self.train_ready_data_path, dtype=self.dtype)
