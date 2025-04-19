2adeb4d9402030767119b8c5c93c38103592989b
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2025-04-19 23:09:31 +0200

    ~

--- wind_forecasting/run_scripts/tuning.py
+++ wind_forecasting/run_scripts/tuning.py
@@ -136,7 +136,7 @@ class MLTuningObjective:
         self.log_gpu_stats(stage=f"Trial {trial.number} Start")
 
         # TODO HIGH setup for resample_freq, per_turbine, rank
-        params = self.estimator_class.get_params(trial, self.tuning_phase)
+        params = self.estimator_class.get_params(trial)
         
         if "resample_freq" in params or "per_turbine" in params:
             self.data_module.freq = f"{params["resample_freq"]}s"
