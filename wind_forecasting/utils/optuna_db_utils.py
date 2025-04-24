78a4e7a7de78f777e8f16447e14e044103acc6aa
Aoife Henry <aoife.henry@colorado.edu>
2025-04-24 11:42:09 -0600

    upd

--- wind_forecasting/utils/optuna_db_utils.py
+++ wind_forecasting/utils/optuna_db_utils.py
@@ -226,7 +226,6 @@ def setup_sqlite(sqlite_storage_dir, study_name, restart_tuning, rank):
     """
     # Construct the SQLite URL based on config
     db_path = os.path.join(sqlite_storage_dir, f'{study_name}.db')
-    logging.info(f"rank == {rank} and restart_tuning == {restart_tuning}")
     if rank == 0 and restart_tuning:
         if os.path.exists(db_path):
             logging.info(f"Deleting existing SQLite storage at {db_path}.")
