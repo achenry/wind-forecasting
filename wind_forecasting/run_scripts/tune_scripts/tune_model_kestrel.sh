8c2de1458cb04a16efd1f4259fe3c0bbfdaf678b
Aoife Henry <ahenry@kl6.head.cm.kestrel.hpc.nrel.gov>
2025-04-26 08:24:18 -0600

    upd

--- wind_forecasting/run_scripts/tune_scripts/tune_model_kestrel.sh
+++ wind_forecasting/run_scripts/tune_scripts/tune_model_kestrel.sh
@@ -1,15 +1,13 @@
 #!/bin/bash 
 #SBATCH --account=ssc
-#SBATCH --time=01:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=%j-%x.out
 ##SBATCH --partition=debug
 #SBATCH --nodes=1 # this needs to match Trainer(num_nodes...)
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks-per-node=2 # this needs to match Trainer(devices=...)
+##SBATCH --mem-per-cpu=240G
 #SBATCH --mem-per-cpu=85G
-
-##SBATCH --mem=0 # refers to CPU (not GPU) memory, automatically given all GPU memory in a SLURM job, 85G
-#SBATCH --ntasks=1
 
 # salloc --account=ssc --time=01:00:00 --gpus=2 --ntasks-per-node=2 --partition=debug
 
@@ -23,8 +21,8 @@ export CONFIG_FILE=$2
 # --- Base Directories ---
 export TUNING_PHASE=1
 export BASE_DIR="/home/ahenry/toolboxes/wind_forecasting_env/wind-forecasting"
-export LOG_DIR="${BASE_DIR}/run_scripts/tune_scripts/logs"
 export WORK_DIR="${BASE_DIR}/wind_forecasting"
+export LOG_DIR="${WORK_DIR}/run_scripts/tune_scripts/logs"
 export RESTART_TUNING_FLAG="--restart_tuning" # "" Or "--restart_tuning"
 export AUTO_EXIT_WHEN_DONE="true"  # Set to "true" to exit script when all workers finish, "false" to keep running until timeout
 export NUMEXPR_MAX_THREADS=128
@@ -79,7 +77,7 @@ ml mamba
 ml cuda
 echo "Modules loaded."
 
-eval "$(mamba shell.bash hook)"
+# eval "$(mamba shell.bash hook)"
 mamba activate wind_forecasting_env
 echo "Conda environment 'wind_forecasting_env' activated."
 # --- End Main Environment Setup ---
@@ -92,6 +90,8 @@ else
   echo "ERROR: WANDB API‑key file not found at ${API_FILE}" >&2
   exit 1
 fi
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 echo "=== STARTING PARALLEL OPTUNA TUNING WORKERS ==="
 date +"%Y-%m-%d %H:%M:%S"
@@ -109,6 +109,7 @@ for i in $(seq 0 $((${NUM_GPUS}-1))); do
       # Create a unique seed for this worker
       export CURRENT_WORKER_SEED=$((12 + i*100)) # Base seed + offset per worker (increased multiplier to avoid trials overlap on workers)
       
+      echo "Saving output for worker ${i} to '${LOG_DIR}/slurm_logs/${SLURM_JOB_ID}/worker_${i}_${SLURM_JOB_ID}.log'"
       echo "Starting worker ${i} on assigned GPU ${i} with seed ${CURRENT_WORKER_SEED}"
       export WORKER_RANK=${i}          # Export rank for Python script
       # Launch worker in the background using nohup and a dedicated bash shell
@@ -137,6 +138,7 @@ for i in $(seq 0 $((${NUM_GPUS}-1))); do
       # Note: PYTHONPATH and WANDB_DIR are inherited via export from parent script
 
       echo \"Worker ${i}: Running python script with WORKER_RANK=${WORKER_RANK}...\"
+      
       # --- Run the tuning script ---
       # Workers connect to the already initialized study using the PG URL
       # Pass --restart_tuning flag from the main script environment
