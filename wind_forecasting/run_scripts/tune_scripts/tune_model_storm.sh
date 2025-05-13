49727678e1a740c1067c6c8e9d05d8d14086bffa
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2025-05-13 18:58:45 +0200

    ~

--- wind_forecasting/run_scripts/tune_scripts/tune_model_storm.sh
+++ wind_forecasting/run_scripts/tune_scripts/tune_model_storm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition=all_gpu.p          # Partition for H100/A100 GPUs cfdg.p / all_gpu.p / mpcg.p(not allowed)
+#SBATCH --partition=cfdg.p          # Partition for H100/A100 GPUs cfdg.p / all_gpu.p / mpcg.p(not allowed)
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4         # Match number of GPUs requested below
+#SBATCH --ntasks-per-node=8         # Match number of GPUs requested below
 #SBATCH --cpus-per-task=2           # CPUs per task (4 tasks * 32 = 128 CPUs total) [1 CPU/GPU more than enough]
 #SBATCH --mem-per-cpu=4096          # Memory per CPU (Total Mem = ntasks * cpus-per-task * mem-per-cpu) [flasc uses only ~4-5 GiB max]
-#SBATCH --gres=gpu:H100:4          # Request 2 H100 GPUs
-#SBATCH --time=1-00:00              # Time limit (up to 7 days)
+#SBATCH --gres=gpu:A100:8          # Request 2 H100 GPUs
+#SBATCH --time=7-00:00              # Time limit (up to 7 days)
 #SBATCH --job-name=flasc_tune
 #SBATCH --output=/user/taed7566/Forecasting/wind-forecasting/logs/slurm_logs/flasc_tune_%j.out
 #SBATCH --error=/user/taed7566/Forecasting/wind-forecasting/logs/slurm_logs/flasc_tune_%j.err
