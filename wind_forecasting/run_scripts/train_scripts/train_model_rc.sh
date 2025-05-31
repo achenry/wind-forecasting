2a88b703afc4405c177a6ccc4db8c5431f01b542
Aoife Henry <aoife.henry@colorado.edu>
2025-05-31 12:49:49 -0600

    upd

--- wind_forecasting/run_scripts/train_scripts/train_model_rc.sh
+++ wind_forecasting/run_scripts/train_scripts/train_model_rc.sh
@@ -2,30 +2,37 @@
 #SBATCH --partition=aa100
 #SBATCH --qos=normal
 #SBATCH --nodes=1 # this needs to match Trainer(num_nodes...)
-#SBATCH --qos=normal
-#SBATCH --gres=gpu:1
-#SBATCH --ntasks=1 # necessary for gpus on rc
-#SBATCH --ntasks-per-node=1 # this needs to match Trainer(devices=...)
-##SBATCH --mem-per-cpu=85G TODO what can I ask for here?
-#SBATCH --time=16:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks=4 # necessary for gpus on rc
+#SBATCH --ntasks-per-node=4 # this needs to match Trainer(devices=...)
+#SBATCH --mem-per-cpu=85G
+#SBATCH --time=36:00:00
 #SBATCH --output=%j-%x.log
-
 
 # sinteractive --partition=atesting_a100 --gres=gpu:1 --ntasks-per-node=1 --ntasks=1 --time=50:00
 
 module purge
-module load miniforge
-module load intel impi
-mamba activate wind_forecasting_env
+# module load miniforge
+#module load intel impi
+eval "$(conda shell.bash hook)"
+conda activate wind_forecasting_env
+
+export NUMEXPR_MAX_THREADS=128
+export MODEL=$1
+export MODEL_CONFIG_FILE=$2
+
+API_FILE="../.wandb_api_key"
+if [ -f "${API_FILE}" ]; then
+  source "${API_FILE}"
+else
+  echo "ERROR: WANDB API‑key file not found at ${API_FILE}" >&2
+  exit 1
+fi
 
 echo "SLURM_NTASKS=${SLURM_NTASKS}"
 echo "SLURM_JOB_NUM_NODES=${SLURM_JOB_NUM_NODES}"
 echo "SLURM_GPUS_ON_NODE=${SLURM_GPUS_ON_NODE}"
-export WANDB_API_KEY= HIDDEN
-export PYTHON_EXECUTABLE=$(which python)
+echo "SLURM_JOB_GPUS=${SLURM_JOB_GPUS}"
+echo "SLURM_JOB_GRES=${SLURM_JOB_GRES}"
 
-# NOTE run the following first: python load_data.py --config ../../examples/inputs/training_inputs_rc_awaken.yaml --reload
-# mpirun -np $SLURM_NTASKS 
-srun $PYTHON_EXECUTABLE ../run_model.py --config ../../examples/inputs/training_inputs_rc_flasc.yaml --mode train --model $1 
-# srun python informer.py
-#python train_spacetimeformer.py spacetimeformer windfarm --debug --run_name spacetimeformer_windfarm_debug --context_points 600 --target_points 600
+srun python ../run_model.py --config $MODEL_CONFIG_FILE --mode train --model $MODEL --checkpoint best --override model.x.lr=1.0e-4 model.x.weight_decay=1.0e-8 #--use_tuned_parameters
