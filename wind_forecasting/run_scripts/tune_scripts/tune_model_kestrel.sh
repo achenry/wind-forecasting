27745a4913559cf486f70e1f7ed32680bfb1570e
Aoife Henry <ahenry@kl5.head.cm.kestrel.hpc.nrel.gov>
2025-06-12 08:53:58 -0600

    upd

--- wind_forecasting/run_scripts/tune_scripts/tune_model_kestrel.sh
+++ wind_forecasting/run_scripts/tune_scripts/tune_model_kestrel.sh
@@ -1,18 +1,17 @@
 #!/bin/bash 
 #SBATCH --account=ssc
 #SBATCH --output=%j-%x.out
-#SBATCH --partition=debug
-#SBATCH --time=01:00:00
-#SBATCH --nodes=1 # this needs to match Trainer(num_nodes...)
-#SBATCH --ntasks-per-node=104 # this needs to match Trainer(devices=...)
+##SBATCH --partition=debug
+##SBATCH --time=01:00:00
+##SBATCH --nodes=1 # this needs to match Trainer(num_nodes...)
 ##SBATCH --gres=gpu:2
 ##SBATCH --ntasks-per-node=2 # this needs to match Trainer(devices=...)
-#SBATCH --mem-per-cpu=40G
+##SBATCH --mem-per-cpu=40G
 #SBATCH --nodes=1 # this needs to match Trainer(num_nodes...)
-##SBATCH --time=96:00:00
-##SBATCH --gres=gpu:4
-##SBATCH --ntasks-per-node=4 # this needs to match Trainer(devices=...)
-##SBATCH --mem-per-cpu=85G
+#SBATCH --time=96:00:00
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=4 # this needs to match Trainer(devices=...)
+#SBATCH --mem-per-cpu=40G
 
 # salloc --account=ssc --time=01:00:00 --gpus=2 --ntasks-per-node=2 --partition=debug
 
