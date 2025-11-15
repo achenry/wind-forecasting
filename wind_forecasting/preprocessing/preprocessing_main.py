2a31456a454209626393f84e4bf8bdfc8ad31223
Aoife Henry <aoife.henry@colorado.edu>
2025-11-15 14:43:31 -0800

    upd

--- wind_forecasting/preprocessing/preprocessing_main.py
+++ wind_forecasting/preprocessing/preprocessing_main.py
@@ -658,14 +658,14 @@ def main():
         # identifies when turbine is shut down, filtering for normal turbine operation
         fp = config["processed_data_path"].replace(".parquet", "_out_of_window.npy")
         if args.reload_data or args.regenerate_filters or not os.path.exists(fp):
-            # data_filter.multiprocessor = None
+            data_filter.multiprocessor = None
             out_of_window = data_filter.multi_generate_filter(df_query=df_query, filter_func=data_filter._single_generate_window_range_filter,
                                                                 feature_types=["wind_speed", "power_output"], turbine_ids=data_loader.turbine_ids,
                                                                 window_start=config["filters"]["window_range_flag"]["window_start"], 
                                                                 window_end=config["filters"]["window_range_flag"]["window_end"], 
                                                                 value_min=config["filters"]["window_range_flag"]["value_min"] * data_inspector.rated_turbine_power, 
                                                                 value_max=config["filters"]["window_range_flag"]["value_max"] * data_inspector.rated_turbine_power)
-            # data_filter.multiprocessor = args.multiprocessor
+            data_filter.multiprocessor = args.multiprocessor
             
             with open(fp, "wb") as fpnt:
                 np.save(fpnt, out_of_window)
